dd352722618cccff8e194ab54d24d91350e82ba9
Steven L <steven.s.llamas@gmail.com>
2026-07-21 21:45:52 -0500

    Add name and price fields to Product entity

--- src/main/java/com/store_api_example/entities/Product.java
+++ src/main/java/com/store_api_example/entities/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @Entity
@@ -13,4 +15,10 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price ")
+    private BigDecimal price;
 }
