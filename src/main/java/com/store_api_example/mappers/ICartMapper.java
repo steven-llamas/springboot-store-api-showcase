4486d5abc40c1054f86ea2955a73d5d36e61aa98
Steven L <steven.s.llamas@gmail.com>
2026-07-21 21:46:42 -0500

    Add CartItem to CartItemDto mapping

--- src/main/java/com/store_api_example/mappers/ICartMapper.java
+++ src/main/java/com/store_api_example/mappers/ICartMapper.java
@@ -1,11 +1,17 @@
 package com.store_api_example.mappers;
 
 import com.store_api_example.dtos.CartDto;
+import com.store_api_example.dtos.CartItemDto;
+import com.store_api_example.dtos.CartProductDto;
 import com.store_api_example.entities.Cart;
+import com.store_api_example.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ICartMapper {
     CartDto toDto(final Cart cart);
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
+    CartItemDto toDto(final CartItem cartItem);
     Cart    toEntity(final CartDto cartDto);
 }
