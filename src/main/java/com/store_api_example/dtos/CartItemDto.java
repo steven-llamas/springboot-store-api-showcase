5f3b0a49b33b2d6fffad214da3a4852ec462093b
Steven L <steven.s.llamas@gmail.com>
2026-07-21 21:46:12 -0500

    Add product, quantity, totalPrice fields to CartItemDto

--- src/main/java/com/store_api_example/dtos/CartItemDto.java
+++ src/main/java/com/store_api_example/dtos/CartItemDto.java
@@ -1,4 +1,12 @@
 package com.store_api_example.dtos;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class CartItemDto {
+    private CartProductDto  product;
+    private Long            quantity;
+    private BigDecimal      totalPrice;
 }
